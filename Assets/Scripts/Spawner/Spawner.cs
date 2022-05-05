a364558419ba6d0658178d20bb05b439e63d7eec
Alexander <sandkovic@gmail.com>
2022-05-05 23:25:02 +0500

    big update
    added:
    + controls
    + tail generation
    + snake collision and reaction on it
    + bonus mechanics
    + line spawner

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _container;
+    [SerializeField] private int _repeatCount;
+    [SerializeField] private int _distanceBetweenFullLine;
+    [SerializeField] private int _distanceBetweenRandomLine;
+
+    [SerializeField] private Block _blockTemplate;
+    [SerializeField] private int _blockSpawnChance;
+
+    private SpawnPoint[] _blockSpawnPoints;
+
+    private void Start()
+    {
+        _blockSpawnPoints = GetComponentsInChildren<SpawnPoint>();
+
+        for (int i = 0; i < _repeatCount; i++)
+        {
+            MoveSpawner(_distanceBetweenFullLine);
+            GenerateFullLine(_blockSpawnPoints, _blockTemplate.gameObject);
+            MoveSpawner(_distanceBetweenRandomLine);
+            GenerateRandomElements(_blockSpawnPoints, _blockTemplate.gameObject, _blockSpawnChance);
+        }
+    }
+
+    private void GenerateFullLine(SpawnPoint[] spawnPoints, GameObject generatedElement)
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            GenerateElement(spawnPoints[i].transform.position, generatedElement);
+        }
+    }
+
+    private void GenerateRandomElements(SpawnPoint[] spawnPoints, GameObject generatedElement, int spawnChance)
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (Random.Range(0, 100) < spawnChance)
+            {
+                GameObject element = GenerateElement(spawnPoints[i].transform.position, generatedElement);
+            }
+        }
+    }
+
+    private GameObject GenerateElement(Vector3 spawnPoint, GameObject generatedElement)
+    {
+        spawnPoint.y -= generatedElement.transform.localScale.y;
+        return Instantiate(generatedElement, spawnPoint, Quaternion.identity, _container);
+    }
+
+    private void MoveSpawner(int distanceY)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + distanceY, transform.position.z);
+    }
+}
